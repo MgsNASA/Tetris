d0386649437ef5da194a9b4648e4e290306bfc3f
MgsNASA <106309942+MgsNASA@users.noreply.github.com>
2024-09-13 10:40:44 +0600

    Change

--- Assets/Scripts/TetrisBlock.cs
+++ Assets/Scripts/TetrisBlock.cs
@@ -1,10 +1,60 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
-public class TetrisBlock : Block
+public class TetrisBlock : MonoBehaviour
 {
-    
+
+    public Transform pivot;
+    Block [,] gridTetris;
+    public void Rotate( )
+    {
+        // Реализация вращения
+    }
+
+    public void Move( Vector3 direction )
+    {
+        transform.position += direction;  // Добавляем направление к текущей позиции
+        Debug.Log ( "New block position: " + transform.position );  // Лог для проверки новой позиции
+
+    }
+
+    public bool IsValidPosition( )
+    {
+        foreach ( Transform child in transform )
+        {
+            Vector3 pos = child.position;
+
+            // Проверка на выход за границы поля
+            if ( pos.x < 0 || pos.x >= Helper.WIDTH || pos.y < 0 || pos.y >= Helper.HEIGHT )
+            {
+                return false;
+            }
+
+            // Проверка на пересечение с другими блоками
+            if ( gridTetris [ ( int ) pos.y , ( int ) pos.x ] != null )
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public void Init( Block [ , ] grid )
+    {
+        pivot = transform;
+        gridTetris = grid;
+        // Логика инициализации
+    }
+
+    public void HardDrop( )
+    {
+        // Логика быстрой посадки блока
+    }
+
+    public Block GetDeadBlock( )
+    {
+        // Логика получения "мёртвого" блока
+        return new Block ();
+    }
+
 }
---
+++ Assets/Scripts/TetrisBlockManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlockManager
+{
+    private GameController gameController;
+    public TetrisBlock currBlock;
+    private GhostBlock ghostBlock;
+    private int nextBlock;
+
+    public TetrisBlockManager( GameController controller )
+    {
+        gameController = controller;
+    }
+
+    public void NewBlock( )
+    {
+        currBlock = GameObject.Instantiate ( gameController.Blocks [ nextBlock ] , gameController.startPos , Quaternion.identity );
+        currBlock.Init ( gameController.grid );
+        currBlock.pivot.position += gameController.Pivots [ nextBlock ];
+        
+        currBlock.HardDrop ();
+        GhostBlockImgUpdate ();
+
+    }
+
+    public void EndTurn( )
+    {
+        // Логика окончания хода
+    }
+
+    public void GameOver( )
+    {
+        // Логика окончания игры
+    }
+
+    public void GameClear( )
+    {
+        // Логика победы в игре
+    }
+
+    public void NextBlock( )
+    {
+        nextBlock = Random.Range ( 0 , gameController.Blocks.Length );
+        GhostBlockImgUpdate ();
+    }
+
+    public void HorizontalMove( Vector3 direction )
+    {
+        Debug.Log ( "Horizontal Move called with direction: " + direction );
+
+        // Сначала передвигаем блок
+        currBlock.Move ( direction );
+
+        // Проверяем, валидна ли новая позиция
+        if ( !currBlock.IsValidPosition () )
+        {
+            // Если позиция не валидна, отменяем движение
+            currBlock.Move ( -direction );
+            Debug.Log ( "Invalid position! Move cancelled." );
+        }
+        else
+        {
+            GhostBlockImgUpdate ();
+        }
+    }
+
+    public void VerticalMove( Vector3 direction )
+    {
+        Debug.Log ( "Vertical Move called with direction: " + direction );
+
+        // Передвигаем блок
+        currBlock.Move ( direction );
+
+        // Проверка допустимости
+        if ( !currBlock.IsValidPosition () )
+        {
+            // Если движение не допустимо, отменяем
+            currBlock.Move ( -direction );
+            Debug.Log ( "Invalid position! Move cancelled." );
+        }
+
+    }
+
+    public void Rotate( )
+    {
+        currBlock.Rotate ();
+    }
+
+    public bool ValidMove( )
+    {
+        return currBlock.IsValidPosition ();
+    }
+
+    public void GhostBlockImgUpdate( )
+    {
+        // Обновление визуализации Ghost блока
+    }
+
+    public Block GetDeadBlock( )
+    {
+        return currBlock.GetDeadBlock ();
+    }
+}
